65df0a52b737db6f983f2aa70c08b05d060a4ccd
Constantin Sidorov <52686993+Normal66@users.noreply.github.com>
2023-03-24 23:25:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SC = os.urandom(30).hex()
     SECRET_KEY = SC
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://steamup:Ytu0lzq_2022@127.0.0.1:5432/steamup'
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     THREADS_PER_PAGE = 8
@@ -16,4 +15,4 @@ class Config(object):
     ALLOWED_EXTENSIONS = {'csv', 'txt'}
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024
     UPLOAD_EXTENSIONS = ['.txt']
-
+    
